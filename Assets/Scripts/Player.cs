af5def0787201725f9c69e76d10573e5365e2e8c
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-09-26 17:32:33 +0100

    docs(player-movement): Added comments to the PlayerMovement script to explain what is happening.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    // [SerializeField] means it can be edited in the editor. Without having to declare the variable as public.
     [SerializeField] private float moveSpeed = 7f;
 
     private void Update() {
+
+        // Vector2 called inputVector, inizialised as a Vector2.zero.
         Vector2 inputVector = new Vector2(0, 0);
 
+        // Old unity input system
         if (Input.GetKey(KeyCode.W)) {
             inputVector.y += 1;
         } 
@@ -22,10 +26,14 @@ public class Player : MonoBehaviour {
             inputVector.x += 1;
         }
 
+        // Normalizes Vector so that it doesn't go faster when moving in 2 directions.
         inputVector = inputVector.normalized;
 
+        // New Vector3 that uses the inputVector to move on the x and z planes.
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        // transform.position is the position of the gameobject that has the script attached. * by Time.deltaTime to make the movement fps independent.
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+
     }
 
 }
