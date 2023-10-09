9eb595fd22e23bcb2f88a26825614ee7d4f6a77c
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-09 20:04:41 +0100

    feat(plate-list): Implemented a list for the plate that shows the current KitchenObjects on the plate. Also added logic to add KitchenObjects to the plate from the ClearCounter, CuttingCounter and StoveCounter. You can also place KitchenObjects onto the plate on the ClearCounter. Finally an additional list is used to know what KitchenObjects the plate can contain.

--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -127,7 +127,29 @@ public class StoveCounter : BaseCounter, IHasProgress
         }
         else
         {
-            if (!player.HasKitchenObject())
+            if (player.HasKitchenObject())
+            {
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    {
+                        GetKitchenObject().DestroySelf();
+
+                        state = State.Idle;
+
+                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs
+                        {
+                            state = state,
+                        });
+
+                        OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs
+                        {
+                            progressNormalized = 0f
+                        });
+                    }
+                }
+            }
+            else
             {
                 GetKitchenObject().SetKitchenObjectParent(player);
                 state = State.Idle;
