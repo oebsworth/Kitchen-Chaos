b201346574040f71f2538f226584c9bebd1f064f
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-10 20:12:22 +0100

    feat(delivery-manager): Implemented the DeliveryManager, RecipeSO and RecipeListSO. This prints to the console the current recipes avaliable and whether or not a recipe was correct when delivered. Also implemented temporary controller control scheme.

--- Assets/Scripts/Counters/DeliveryCounter.cs
+++ Assets/Scripts/Counters/DeliveryCounter.cs
@@ -10,6 +10,8 @@ public class DeliveryCounter : BaseCounter
         {
             if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
             {
+                // Only accepts Plates.
+                DeliveryManager.Instance.DeliverRecipe(plateKitchenObject);
                 player.GetKitchenObject().DestroySelf();
             }
         }
--- Assets/Scripts/Counters/StoveCounter.cs
+++ Assets/Scripts/Counters/StoveCounter.cs
@@ -94,7 +94,6 @@ public class StoveCounter : BaseCounter, IHasProgress
 
                     break;
             }
-            Debug.Log(state);
         }
     }
 
