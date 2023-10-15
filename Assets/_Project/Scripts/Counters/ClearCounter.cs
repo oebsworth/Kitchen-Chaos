0de252037d1713f8364e8237d6150e74a609f64c
Oliver Ebsworth <ebswortholiver@gmail.com>
2023-10-15 20:01:09 +0100

    docs(documentation-comments): Added documentation comments for ClearCounter and StoveCounterVisual scripts. Also added vscode folder.

--- Assets/_Project/Scripts/Counters/ClearCounter.cs
+++ Assets/_Project/Scripts/Counters/ClearCounter.cs
@@ -1,43 +1,54 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ClearCounter : BaseCounter
 {
-    [SerializeField] private KitchenObjectSO kitchenObjectSO;
-
+    // Public class called ClearCounter that inherits from BaseCounter.
     public override void Interact(Player player)
     {
-        if (!HasKitchenObject())
+        // Override of the Interact function that derives from the BaseCounter class. 
+        if (this.HasKitchenObject())
         {
+            // ClearCounter has a KitchenObject.
             if (player.HasKitchenObject())
             {
-                player.GetKitchenObject().SetKitchenObjectParent(this);
-            }
-        }
-        else
-        {
-            if (player.HasKitchenObject())
-            {
+                // Player is holding a KitchenObject.
                 if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
                 {
-                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    // Player is holding a PlateKitchenObject.
+                    if (plateKitchenObject.TryAddIngredient(this.GetKitchenObject().GetKitchenObjectSO()))
                     {
-                        GetKitchenObject().DestroySelf();
+                        // The ingredient on the ClearCounter is suitable to be added to the PlateKitchenObject that the Player is holding.
+                        this.GetKitchenObject().DestroySelf(); // Gets the KitchenObject on the ClearCounter and deletes it.
                     }
                 }
                 else
                 {
-                    if (GetKitchenObject().TryGetPlate(out plateKitchenObject))
+                    // Player is not holding a PlateKitchenObject.
+                    if (this.GetKitchenObject().TryGetPlate(out plateKitchenObject))
                     {
+                        // ClearCounter is has a PlateKitchenObject.
                         if (plateKitchenObject.TryAddIngredient(player.GetKitchenObject().GetKitchenObjectSO()))
                         {
-                            player.GetKitchenObject().DestroySelf();
+                            // The ingredient on the Player is suitable to be added to the PlateKitchenObject that the ClearCounter has.
+                            player.GetKitchenObject().DestroySelf(); // Gets the KitchenObject on the ClearCounter and deletes it.
                         }
                     }
                 }
             }
             else
             {
-                GetKitchenObject().SetKitchenObjectParent(player);
+                // Player is not holding a KitchenObject.
+                this.GetKitchenObject().SetKitchenObjectParent(player); // Gets the KitchenObject on the ClearCounter and moves it to the Player.
+            }
+        }
+        else
+        {
+            // ClearCounter does not have a KitchenObject.
+            if (player.HasKitchenObject())
+            {
+                // Player is holding a KitchenObject.
+                player.GetKitchenObject().SetKitchenObjectParent(this); // Gets the KitchenObject on the Player and moves it to the ClearCounter.
             }
         }
     }
--- Assets/_Project/Scripts/Counters/StoveCounterVisual.cs
+++ Assets/_Project/Scripts/Counters/StoveCounterVisual.cs
@@ -4,20 +4,25 @@ using UnityEngine;
 
 public class StoveCounterVisual : MonoBehaviour
 {
+    // Public class called StoveCounterVisual that inherits from MonoBehaviour.
+
     [SerializeField] private StoveCounter stoveCounter;
     [SerializeField] private GameObject stoveOnGameObject;
     [SerializeField] private GameObject particlesGameObject;
+    // References to the current StoveCounter and GameObjects within the prefab.
 
     private void Start()
     {
-        stoveCounter.OnStateChanged += StoveCounter_OnStateChanged;
+        // Unity event function that is called after Awake.
+        stoveCounter.OnStateChanged += StoveCounter_OnStateChanged; // Listens to an event from the StoveCounter called OnStateChanged.
     }
 
     private void StoveCounter_OnStateChanged(object sender, StoveCounter.OnStateChangedEventArgs e)
     {
-        bool showVisual = e.state == StoveCounter.State.Frying || e.state == StoveCounter.State.Fried;
-        stoveOnGameObject.SetActive(showVisual);
-        particlesGameObject.SetActive(showVisual);
+        // OnStateChanged event function that is called based on the event from the StoveCounter.
+        bool showVisual = e.state == StoveCounter.State.Frying || e.state == StoveCounter.State.Fried; // Sets a bool based on whether or not State sent from the StoveCounter event is Frying or Fried.
+        stoveOnGameObject.SetActive(showVisual); // Shows the red glow on the StoveCounter when the showVisual bool is set to true
+        particlesGameObject.SetActive(showVisual); // Shows particles on the StoveCounter when the showVisual bool is set to true.
     }
 }
 
